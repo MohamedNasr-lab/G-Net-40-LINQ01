8cd5d4f90c869203eafb525b1d3ebb86ee7eaf6e
MohamedNasr <mohamednasr27122@gmail.com>
2026-04-05 18:50:48 +0200

    Q2

--- wadasdadw/Program.cs
+++ wadasdadw/Program.cs
@@ -10,16 +10,25 @@ namespace wadasdadw
     {
         static void Main(string[] args)
         {
-            
 
-            
+
+
 
             #region Q1
-            var result = Source.ProductList.Where(c => c.Category == "Seafood");
-            foreach (var product in result)
+            //var result = Source.ProductList.Where(c => c.Category == "Seafood");
+            //foreach (var product in result)
+            //{
+            //    Console.WriteLine($"product name is {product.ProductName} ");
+            //    Console.WriteLine($"product price is {product.UnitPrice} ");
+            //}
+            #endregion
+
+            #region Q2
+            var result = Source.ProductList.Select(p => p.ProductName);
+
+            foreach (var name in result)
             {
-                Console.WriteLine($"product name is {product.ProductName} ");
-                Console.WriteLine($"product price is {product.UnitPrice} ");
+                Console.WriteLine(name);
             }
             #endregion
 
