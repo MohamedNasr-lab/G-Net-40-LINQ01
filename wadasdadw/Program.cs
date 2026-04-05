3eb83bcd7420b4b65c5448dab924463349bf3d74
MohamedNasr <mohamednasr27122@gmail.com>
2026-04-05 18:35:04 +0200

    Add project files.

---
+++ wadasdadw/Program.cs
@@ -0,0 +1,17 @@
+﻿using static wadasdadw.Source;
+using static wadasdadw.Product;
+using static wadasdadw.Customer;
+using static wadasdadw.Employee;
+using static wadasdadw.Order;
+using static wadasdadw.Student;
+namespace wadasdadw
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+        }
+    }
+}
